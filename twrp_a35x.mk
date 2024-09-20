71c7ab754a06e3099e49a3e826dfd8283a2d598b
Jamie Falconer <mrfluffyoven@gmail.com>
2024-09-20 09:08:22 +0100

    omni > twrp

--- twrp_a35x.mk
+++ twrp_a35x.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a35x device
 $(call inherit-product, device/samsung/a35x/device.mk)
 
 PRODUCT_DEVICE := a35x
-PRODUCT_NAME := omni_a35x
+PRODUCT_NAME := twrp_a35x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A356U1
 PRODUCT_MANUFACTURER := samsung
