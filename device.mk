2f032e3fe8e45e788c10d95d803de6bd8943c06d
Jamie Falconer <mrfluffyoven@gmail.com>
2024-09-20 08:58:07 +0100

    treewide cleanup

--- device.mk
+++ device.mk
@@ -6,3 +6,32 @@
 #
 
 LOCAL_PATH := device/samsung/a35x
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload \
+    checkpoint_gc
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 32
+PRODUCT_TARGET_VNDK_VERSION := 32
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
